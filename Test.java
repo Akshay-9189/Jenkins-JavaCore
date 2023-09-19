a94a3dc970c92790a8bdb33f14815f6ce56b63c4
Akshay-9189 <akshaykumbhar957@gmail.com>
2023-09-19 19:04:53 +0530

    changes

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("This is first build");
-        System.out.println("Hello Jenkins !!!");
         System.out.println("Jenkins CICD");
+        System.out.println("Automatic Build and Deploy");
     }
 }
